80ca588586b289bc418ffe48d588e50836d1f9ba
Matti Yli-Ojanperä <matti.yli-ojanpera@aalto.fi>
2016-01-14 10:12:28 +0200

    writed few views

--- skeletor/gameshop/views.py
+++ skeletor/gameshop/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Game, User, Score
 
-# Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'gameshop/index.html', context)
+
+''' TODO write corresponding templates!
+
+def register(request):
+    context = {}
+    return render(request, 'gameshop/register.html', context)
+
+def profile(request):
+    context = {}
+    return render(request, 'gameshop/profile.html', context)
+
+def shop(request):
+    context = {}
+    return render(request, 'gameshop/shop.html', context)
+
+def play(request):
+    context = {}
+    return render(request, 'gameshop/play.html', context)
+
+'''
