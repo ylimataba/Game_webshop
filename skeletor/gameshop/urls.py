1759d35730b6fb3d02b060a3aff9bd0396d3797b
Matti Yli-Ojanperä <matti.yli-ojanpera@aalto.fi>
2016-01-14 10:06:53 +0200

    created gameshop/urls.py and writed few urlpatterns

---
+++ skeletor/gameshop/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url, include
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+
+    ''' TODO write corresponding views!
+    
+    url(r'^register$', views.register, name='register'),
+    url(r'^profile$', views.profile, name='profile'),
+    url(r'^shop$', views.shop, name='shop'),
+    url(r'^play$', views.play, name='play'),
+    
+    '''
+]
+
