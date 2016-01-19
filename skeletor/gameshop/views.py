ed3532c88fed84adee5515ba7247c26261bc8930
Matti Yli-Ojanperä <matti.yli-ojanpera@aalto.fi>
2016-01-19 11:33:56 +0200

    added user authentication verifycation

--- skeletor/gameshop/views.py
+++ skeletor/gameshop/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from .models import Game, User, Score #TODO import correct models
+#from django.contrib.auth.decorators import login_required
 
+#@login_required(login_url='login')
 def index(request):
-    context = {}
+    if not request.user.is_authenticated():
+        context = {'user':False}
+    else:
+        context = {'user':True}
     return render(request, 'gameshop/index.html', context)
 
 ''' TODO write corresponding templates!
