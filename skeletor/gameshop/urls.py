29d3c51a3cfd1ff0e8a9ac9eb57b28ed8aa76401
Amin Modabberian <modabba1@aalto.fi>
2016-01-26 16:04:45 +0200

    päivitetty urls ja views uusia templateja varten

--- skeletor/gameshop/urls.py
+++ skeletor/gameshop/urls.py
@@ -7,7 +7,10 @@ urlpatterns = [
     url(r'^register$', views.register, name='register'),
     url(r'^profile$', views.profile, name='profile'),
     url(r'^shop$', views.shop, name='shop'),
-    url(r'^play$', views.play, name='play'),
+    url(r'^play/(?P<game_id>[0-9]+)/$', views.play, name='play'),
+    url(r'^shop/success$', views.shop_success, name='shop_success'),
+    url(r'^shop/cancel$', views.shop_cancel, name='shop_cancel'),
+    url(r'^shop/error$', views.shop_error, name='shop_error'),
     
 ]
 
