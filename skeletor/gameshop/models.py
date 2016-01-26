f4ff5d4efaacecf57197f2bea73320cd709b1182
Matti Yli-Ojanperä <matti.yli-ojanpera@aalto.fi>
2016-01-26 11:20:14 +0200

    edited Gamer and Developer to extend User model

--- skeletor/gameshop/models.py
+++ skeletor/gameshop/models.py
@@ -3,44 +3,41 @@ from django.contrib.auth.models import User
 import gameshop.genre as game_genres #HUOM! 
 
 class Score(models.Model):
-	"""Model to keep all the scores in database"""
-	
+	""" Model to keep all the scores in database """
 	points = models.PositiveIntegerField()
 	game = models.ForeignKey("Game", related_name="GameScores")
 	user = models.ForeignKey(User, related_name="UserScores")
-	
+
 	class Meta:
 		ordering = ["points"]
 
 class Game(models.Model):
-	"""Model to keep all the game infromation in database"""
-	
+	""" Model to keep all the game infromation in database """
 	name = models.CharField(max_length=225, unique=True)
 	description = models.CharField(max_length=300)
 	release_date = models.DateField(blank=True)
-	game_developer = models.ForeignKey(User, related_name="DeveloperGames") # can't be just developer because it clashes with Developer.library
+	game_developer = models.ForeignKey(User, related_name="DeveloperGames")
 	publisher = models.CharField(max_length=225, blank=True)
-	genre = models.CharField(max_length=225, choices=game_genres.GENRE_CHOICES) #GENRE_CHOICES omassa tiedostossa
+        # GENRE_CHOICES omassa tiedostossa
+	genre = models.CharField(max_length=225, choices=game_genres.GENRE_CHOICES)
 	source = models.URLField(max_length=225, unique=True)
 
 	def get_gamescores(self):
-		return self.DeveloperGames.all()
+		return self.GameScores.all()
 	leaderboard = property(get_gamescores)
 
 	class Meta:
 		ordering =["name"]
 
 class Gamer(models.Model):
-	"""Model to keep all the gamer infromation in database"""
-	user = models.ForeignKey(User)
-	#date_of_birth = models.DateField()
+	""" Extend User model with user.developer field """
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	library = models.ManyToManyField("Game")
 
 
 class Developer(models.Model):
-	"""Model to keep all the developer infromation in database"""
-	user = models.ForeignKey(User)
-	#date_of_birth = models.DateField()
+	""" Extends User model with user.developer field """
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	library = models.ManyToManyField("Game")
 
 	
