5d01c60452cc3e7e75809c437ed9b83788e0cf4f
Matti Yli-Ojanperä <matti.yli-ojanpera@aalto.fi>
2016-03-02 13:51:34 +0200

    added STATIC_ROOT to settings

--- skeletor/skeletor/settings.py
+++ skeletor/skeletor/settings.py
@@ -100,6 +100,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
