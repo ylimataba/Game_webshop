859d9767f57b1c0d10088d1d79c60a02c37dd634
Matti Yli-Ojanperä <matti.yli-ojanpera@aalto.fi>
2016-03-02 14:30:36 +0200

    added STATICFILESDIRS

--- skeletor/skeletor/settings.py
+++ skeletor/skeletor/settings.py
@@ -103,6 +103,8 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
+
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/'
 
